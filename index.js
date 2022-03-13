b7e8ce206758209d6765327d8019b49ac5b74643
maicallatrace <maicallatrace@gmail.com>
2022-03-12 22:02:05 -0700

    REPO

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+    console.log(Mike, JavaScript);
+}
+function introduction(name) {
+    return "Hi, my name is " + name + ".";
+}
+    function introductionWithLanguage(language) {
+        console.log(JavaScript);
+}
+function introductionWithLanguage(name, language) {
+    return "Hi, my name is " + name + " and i am learning to program in " + language + ".";
+}
+  function introductionWithLanguageOptional(name, language = 'JavaScript') {
+    return 'Hi, my name is ' + name + ' and I am learning to program in ' + language  + '.';
+  }
